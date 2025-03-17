211ab5c80e7e8b020e83b1a02a5629a21b95b96a
Eric Stock <eric.stock@K202Domain.CSN.com>
2025-03-17 10:15:08 -0230

    Added templates for website

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-from flask import *
+from flask import Flask, render_template, request
 
-print("Hello Flask")
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/greet", methods=["POST"])
+def greet():
+    card = request.form.get("creditcard", "invalid")
+    return render_template("greet.html", card=card)
