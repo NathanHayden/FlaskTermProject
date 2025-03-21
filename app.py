3e253e658b946d7758b172e3d3efe3f344b75f61
Eric Stock <eric.stock@K202Domain.CSN.com>
2025-03-21 08:34:12 -0230

    Modified project to store information about video games

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "hello_class"
 
-@app.route("/")
+#videoGames = []
+
+@app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    if "videoGames" not in session:
+        print("clearing games")
+        session["videoGames"] = []
 
-@app.route("/greet", methods=["POST"])
-def greet():
-    card = request.form.get("creditcard", "invalid")
-    return render_template("greet.html", card=card)
+    print(session.get("videoGames"))
+    return render_template("index.html",games=session.get("videoGames"))
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "GET":
+        return render_template("add.html")
+    elif request.method == "POST":
+        if "videoGames" not in session:
+            print("Clearing session data")
+            session["videoGames"] = []
+        title = request.form.get("title", "invalid")
+        plat = request.form.get("plat", "invalid")
+        year = request.form.get("year", "invalid")
+        session["videoGames"].append({"title":title, "plat":plat, "year":year})
+        print(session.get("videoGames"))
+        session.modified = True
+        return redirect("/")
+
+
